f8591cb86b63914e193b763791f986a512fa1180
EronAlves1996 <eron.alves@rocketmail.com>
2023-09-19 01:57:56 -0300

    implement a test to test in order traversal

--- implementations/src/test/java/com/eronalves/trees/BinaryTreeTest.java
+++ implementations/src/test/java/com/eronalves/trees/BinaryTreeTest.java
@@ -1,12 +1,36 @@
 package com.eronalves.trees;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class BinaryTreeTest {
 
+  private PrintStream println;
+
+  private ByteArrayOutputStream mockPrintln(){
+    ByteArrayOutputStream bo = new ByteArrayOutputStream();
+
+    // store the default instance here, to restore after
+    println = System.out;
+    System.setOut(new PrintStream(bo));
+
+    return bo;
+  }
+
+  @AfterEach
+  public void restorePrintln (){
+    System.setOut(println);
+  }
 
   @Test
-  public void inOrderTraversalTest(){
+  public void inOrderTraversalTest() throws IOException {
+    ByteArrayOutputStream bo = mockPrintln();
+
     BinaryTree<Integer> integerBinaryTree = new BinaryTree<>();
     integerBinaryTree.root = new BinaryTreeNode<>(1);
     var root = integerBinaryTree.root;
@@ -14,6 +38,11 @@ public class BinaryTreeTest {
     root.right = new BinaryTreeNode<>(3);
 
     integerBinaryTree.inOrderTraversal();
-    // prints 2 1 3
+
+    bo.flush();
+    String printedOut = new String(bo.toByteArray());
+
+    // /r/n is windows default format for line carrier
+    Assertions.assertEquals("2\r\n1\r\n3\r\n", printedOut);
   }
 }
